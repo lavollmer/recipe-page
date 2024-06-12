1bf2ce593b21eab914b9b79fb2867409a896519b
Laura <laura.e.vollmer@gmail.com>
2024-06-12 10:54:20 -0500

    extending theme of colors for tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nutmeg: "hsl(14, 45%, 36%)",
+        "dark-raspberry": "hsl(332, 51%, 32%)",
+        white: "hsl(0, 0%, 100%)",
+        "rose-white": "hsl(330, 100%, 98%)",
+        eggshell: "hsl(30, 54%, 90%)",
+        "light-grey": "hsl(30, 18%, 87%)",
+        "wenge-brown": "hsl(30, 10%, 34%)",
+        "dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
-}
+};
