6a5cb39105412ed30bef1058470395b7d88faea5
Laura <laura.e.vollmer@gmail.com>
2024-06-13 11:35:57 -0500

    fixing nutrition positioning

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,24 @@ function App() {
               </h2>
               {/* list-disc class is used to add bullet points and pl-5 to add padding to left */}
               <ul className="list-disc pl-5">
-                <li><span className="text-dark-charcoal font-bold font-outfit">Total:</span> Approximately 10 minutes</li>
-                <li><span className="text-dark-charcoal font-bold font-outfit">Preparation:</span> 5 minutes</li>
-                <li><span className="text-dark-charcoal font-bold font-outfit">Cooking:</span> 5 minutes</li>
+                <li>
+                  <span className="text-dark-charcoal font-bold font-outfit">
+                    Total:
+                  </span>{" "}
+                  Approximately 10 minutes
+                </li>
+                <li>
+                  <span className="text-dark-charcoal font-bold font-outfit">
+                    Preparation:
+                  </span>{" "}
+                  5 minutes
+                </li>
+                <li>
+                  <span className="text-dark-charcoal font-bold font-outfit">
+                    Cooking:
+                  </span>{" "}
+                  5 minutes
+                </li>
               </ul>
             </div>
           </div>
@@ -56,33 +71,50 @@ function App() {
             </h2>
             <ol className="text-wenge-brown font-outfit list-decimal pl-5">
               <li>
-                <span className="text-dark-charcoal font-bold font-outfit">Beat the eggs:</span> In a bowl, beat the eggs with a pinch of salt and
-                pepper until they are well mixed. You can add a tablespoon of
-                water or milk for a fluffier texture.
+                <span className="text-dark-charcoal font-bold font-outfit">
+                  Beat the eggs:
+                </span>{" "}
+                In a bowl, beat the eggs with a pinch of salt and pepper until
+                they are well mixed. You can add a tablespoon of water or milk
+                for a fluffier texture.
               </li>
               <li>
-              <span className="text-dark-charcoal font-bold font-outfit">Heat the pan:</span> Place a non-stick frying pan over medium heat and
-                add butter or oil. Cook the omelette: Once the butter is melted
-                and bubbling, pour in the eggs.{" "}
+                <span className="text-dark-charcoal font-bold font-outfit">
+                  Heat the pan:
+                </span>{" "}
+                Place a non-stick frying pan over medium heat and add butter or
+                oil. Cook the omelette: Once the butter is melted and bubbling,
+                pour in the eggs.{" "}
               </li>
               <li>
-              <span className="text-dark-charcoal font-bold font-outfit">Cook the omelette:</span> Once the butter is melted and bubbling, pour
-                in the eggs.{" "}
+                <span className="text-dark-charcoal font-bold font-outfit">
+                  Cook the omelette:
+                </span>{" "}
+                Once the butter is melted and bubbling, pour in the eggs.{" "}
               </li>
               <li>
-              <span className="text-dark-charcoal font-bold font-outfit">Add fillings (optional):</span> When the eggs begin to set at the edges
-                but are still slightly runny in the middle, sprinkle your chosen
-                fillings over one half of the omelette.{" "}
+                <span className="text-dark-charcoal font-bold font-outfit">
+                  Add fillings (optional):
+                </span>{" "}
+                When the eggs begin to set at the edges but are still slightly
+                runny in the middle, sprinkle your chosen fillings over one half
+                of the omelette.{" "}
               </li>
               <li>
-              <span className="text-dark-charcoal font-bold font-outfit">Fold and serve:</span> As the omelette continues to cook, carefully
-                lift one edge and fold it over the fillings. Let it cook for
-                another minute, then slide it onto a plate.
+                <span className="text-dark-charcoal font-bold font-outfit">
+                  Fold and serve:
+                </span>{" "}
+                As the omelette continues to cook, carefully lift one edge and
+                fold it over the fillings. Let it cook for another minute, then
+                slide it onto a plate.
               </li>
               <li>
-              <span className="text-dark-charcoal font-bold font-outfit">Enjoy:</span> Serve hot, with additional salt and pepper if needed.
-                Nutrition The table below shows nutritional values per serving
-                without the additional fillings.
+                <span className="text-dark-charcoal font-bold font-outfit">
+                  Enjoy:
+                </span>{" "}
+                Serve hot, with additional salt and pepper if needed. Nutrition
+                The table below shows nutritional values per serving without the
+                additional fillings.
               </li>
             </ol>
           </div>
@@ -95,18 +127,27 @@ function App() {
               The table below shows nutritional values per serving without the
               additional fillings.
             </p>
-            <p className="text-dark-charcoal font-outfit">Calories</p>
-            <p className="text-nutmeg font-bold font-outfit">277kcal</p>
-            <hr className="border-t border-gray-400 mx-10 my-4" />
-            <p className="text-dark-charcoal font-outfit">Carbs</p>
-            <p className="text-nutmeg font-bold font-outfit">0g</p>
-            <hr className="border-t border-gray-400 mx-10 my-4" />
-            <p className="text-dark-charcoal font-outfit">Protein</p>
-            <p className="text-nutmeg font-bold font-outfit">20g</p>
-            <hr className="border-t border-gray-400 mx-10 my-4" />
-            <p className="text-dark-charcoal font-outfit">Fat</p>
-            <p className="text-nutmeg font-bold font-outfit">22g</p>
-            <hr className="border-t border-gray-400 mx-10 my-4" />
+            <div className="flex">
+              <div>
+                <p className="text-dark-charcoal font-outfit">Calories</p>
+                <p className="text-nutmeg font-bold font-outfit">277kcal</p>
+              </div>
+              <hr className="border-t border-gray-400 mx-10 my-4" />
+              <div>
+                <p className="text-dark-charcoal font-outfit">Carbs</p>
+                <p className="text-nutmeg font-bold font-outfit">0g</p>
+              </div>
+              <hr className="border-t border-gray-400 mx-10 my-4" />
+              <div>
+                <p className="text-dark-charcoal font-outfit">Protein</p>
+                <p className="text-nutmeg font-bold font-outfit">20g</p>
+              </div>
+              <hr className="border-t border-gray-400 mx-10 my-4" />
+              <div>
+                <p className="text-dark-charcoal font-outfit">Fat</p>
+                <p className="text-nutmeg font-bold font-outfit">22g</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
